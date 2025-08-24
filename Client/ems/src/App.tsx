a0580aea135109ef72fa5c2f475f6b6147f86c4f
Kamal Banjade <kamalbanjade67@gmail.com>
2025-08-24 07:54:22 +0545

    Changed browseroute to hasroute

--- Client/ems/src/App.tsx
+++ Client/ems/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, type FC, Component } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
